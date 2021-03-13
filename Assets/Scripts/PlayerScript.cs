98740edf2b4c1135e7ce74e33715faedb66bfa36
Joe Petrakovich <jpetrakovich@gmail.com>
2021-03-13 11:34:00 -0600

    added a player object with basic movement

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(50, 50);
+
+    private Vector2 movement;
+    private Rigidbody2D rigidBodyComponent;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        movement = new Vector2(
+            speed.x * inputX,
+            speed.y * inputY);
+    }
+
+    void FixedUpdate()
+    {
+        if (rigidBodyComponent == null)
+        {
+            rigidBodyComponent = GetComponent<Rigidbody2D>();
+        }
+
+        rigidBodyComponent.velocity = movement;
+    }
+
+}
